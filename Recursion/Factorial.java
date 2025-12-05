10b618a7b8da73cde1fa13b1b2701529b0df7bc2
Shani Kumar Payasi <panditshanipayasi@gmail.com>
2025-12-05 09:04:51 +0530

    Initial commit

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,1 +1,19 @@
-// Sample code for Factorial.java
+public class Factorial {
+  public static int  Factorial(int n){
+    if (n==1){
+      return 1 ;
+    }
+    int fnm=Factorial(n-1);
+    int F= n+Factorial(n-1);
+    return F;
+    
+   
+  }
+  public static void main(String[] args) {
+    int n=2;
+ System.out.println(Factorial(n));
+    
+  }
+
+  
+}
--- Recursion/printDicrementNum.java
+++ Recursion/printDicrementNum.java
@@ -1,19 +1,35 @@
-public class printDicrementNum {
-  public static void  printNum (int num) {
+// public class printDicrementNum {
+//   public static void  printNum (int num) {
 
-    if (num==1){
-      System.out.print(num+" ");
-      return ;
-    }
-    printNum(num-1);
-    System.out.print(num+" ");
+//     if (num==1){
+//       System.out.print(num+" ");
+//       return ;
+//     }
+//     printNum(num-1);
+//     System.out.print(num+" ");
 
     
 
+//   }
+//   public static void main(String[] args) {
+//     int num=10;
+//     printNum(num);
+//   }
+  
+
+// }
+
+public class printDicrementNum {
+  public static void  printNum (int num) {
+    
+
+
+
   }
+
   public static void main(String[] args) {
-    int num=10;
-    printNum(num);
+    
   }
+
   
 }
